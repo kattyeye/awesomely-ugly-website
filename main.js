29bf78de4297a9f375f9c1b27a4b6dad5cc61d48
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-09-13 13:39:26 -0400

    adds nacho libre image

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ let btn2 = document.querySelector('button#btn-2');
     event.target.textContent = "Ew";
     })
 
+document.getElementById('#nacho').addEventListener("click", function (event) {
+    event.target.textContent = "NACHOOOOOOO";
+})
