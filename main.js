c82ed9dd646deed6f0cd5e2db6590eb0d4e29717
Kat Ingram <69052750+kattyeye@users.noreply.github.com>
2021-09-13 14:26:17 -0400

    adds button function to swap images

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ let btn2 = document.querySelector('button#btn-2');
     event.target.textContent = "Ew";
     })
 
-document.getElementById('#nacho').addEventListener("click", function (event) {
-    event.target.textContent = "NACHOOOOOOO";
-})
+document.querySelector('#nacho-btn').addEventListener("click", function changeImage() {
+    var image = document.getElementById('nacho');
+    if (image.src.match("https://m.media-amazon.com/images/I/51hc-naaJuL._AC_SL1059_.jpg")) {
+        image.src = "https://pbs.twimg.com/profile_images/461737379116503040/CaYA3y6e.png";
+    }
+    else {
+        image.src = "https://m.media-amazon.com/images/I/51hc-naaJuL._AC_SL1059_.jpg";
+    }
+});
